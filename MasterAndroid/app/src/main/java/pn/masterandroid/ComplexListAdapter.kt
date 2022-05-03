858dbbe3a353731f53c22d4394420a6be1a72b1a
Patrycjusz Nikrewicz <patrycjusz.nikrewicz@gmail.com>
2022-05-03 12:59:11 +0200

    refactor and changes to the platform similarities

--- MasterAndroid/app/src/main/java/pn/masterandroid/ComplexListAdapter.kt
+++ MasterAndroid/app/src/main/java/pn/masterandroid/ComplexListAdapter.kt
@@ -11,45 +11,46 @@ class ComplexListAdapter(
     private val dataSet: Array<String>
 ) : RecyclerView.Adapter<ComplexListAdapter.ViewHolder>() {
 
-    /**
-     * Provide a reference to the type of views that you are using
-     * (custom ViewHolder).
-     */
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view)
 
-        init {
-            // Define click listener for the ViewHolder's View.
-        }
-    }
-
-    // Create new views (invoked by the layout manager)
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
-        // Create a new view, which defines the UI of the list item
-        val view = LayoutInflater.from(viewGroup.context)
-            .inflate(R.layout.complex_list_item_view_holder, viewGroup, false)
-
-        view.findViewById<ImageView>(R.id.complex_list_item_view_holder_icon).run {
-            AnimationUtils.loadAnimation(context, R.anim.rotate).also { animation ->
-                startAnimation(animation)
+        return ViewHolder(
+            LayoutInflater.from(
+                viewGroup.context
+            ).inflate(
+                R.layout.complex_list_item_view_holder, viewGroup,
+                false
+            ).also { view ->
+                view.findViewById<ImageView>(
+                    R.id.complex_list_item_view_holder_icon
+                ).run {
+                    AnimationUtils.loadAnimation(
+                        context,
+                        R.anim.rotate
+                    ).also { animation ->
+                        startAnimation(
+                            animation
+                        )
+                    }
+                }
             }
-        }
-
-        return ViewHolder(view)
+        )
     }
 
-    // Replace the contents of a view (invoked by the layout manager)
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
-        viewHolder.itemView.findViewById<ImageView>(R.id.complex_list_item_view_holder_icon).run {
-            AnimationUtils.loadAnimation(context, R.anim.rotate).also { animation ->
-                startAnimation(animation)
+        viewHolder.itemView.findViewById<ImageView>(
+            R.id.complex_list_item_view_holder_icon
+        ).run {
+            AnimationUtils.loadAnimation(
+                context,
+                R.anim.rotate
+            ).also { animation ->
+                startAnimation(
+                    animation
+                )
             }
         }
-        // Get element from your dataset at this position and replace the
-        // contents of the view with that element
-//        viewHolder.textView.text = dataSet[position]
     }
 
-    // Return the size of your dataset (invoked by the layout manager)
     override fun getItemCount() = dataSet.size
-
 }
--- MasterAndroid/app/src/main/java/pn/masterandroid/SecondFragment.kt
+++ MasterAndroid/app/src/main/java/pn/masterandroid/SecondFragment.kt
@@ -7,29 +7,30 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 
-/**
- * A simple [Fragment] subclass as the second destination in the navigation.
- */
 class SecondFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false)
+        return inflater.inflate(
+            R.layout.fragment_second,
+            container,
+            false
+        )
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<RecyclerView>(R.id.complex_list_recycler_view).adapter =
-            ComplexListAdapter(
-                arrayListOf<String>().apply {
-                    for (i in 1..999) {
-                        add(i.toString())
-                    }
-                }.toTypedArray()
-            )
+        view.findViewById<RecyclerView>(
+            R.id.complex_list_recycler_view
+        ).adapter = ComplexListAdapter(
+            arrayListOf<String>().apply {
+                for (i in 1..999) {
+                    add(i.toString())
+                }
+            }.toTypedArray()
+        )
     }
 }
