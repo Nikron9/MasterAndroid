7b2d3ced950a4958be2e2a399ca29f47ead994dd
Patrycjusz Nikrewicz <patrycjusz.nikrewicz@gmail.com>
2022-05-04 11:16:34 +0200

    complex animations, components showcase

--- MasterAndroid/app/src/main/java/pn/masterandroid/ComplexListAdapter.kt
+++ MasterAndroid/app/src/main/java/pn/masterandroid/ComplexListAdapter.kt
@@ -13,6 +13,15 @@ class ComplexListAdapter(
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view)
 
+    private val images = listOf(
+        R.drawable.android,
+        R.drawable.flutter,
+        R.drawable.react,
+        R.drawable.xamarin
+    )
+
+    private var animation: Int = 0
+
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder(
             LayoutInflater.from(
@@ -20,36 +29,37 @@ class ComplexListAdapter(
             ).inflate(
                 R.layout.complex_list_item_view_holder, viewGroup,
                 false
-            ).also { view ->
-                view.findViewById<ImageView>(
-                    R.id.complex_list_item_view_holder_icon
-                ).run {
-                    AnimationUtils.loadAnimation(
-                        context,
-                        R.anim.rotate
-                    ).also { animation ->
-                        startAnimation(
-                            animation
-                        )
-                    }
-                }
-            }
+            )
         )
     }
 
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+
+    }
+
+    override fun onViewAttachedToWindow(viewHolder: ViewHolder) {
         viewHolder.itemView.findViewById<ImageView>(
             R.id.complex_list_item_view_holder_icon
         ).run {
+            setImageResource(images[viewHolder.adapterPosition.mod(4)])
+
+            this@ComplexListAdapter.animation = if (viewHolder.adapterPosition.mod(2) == 0) {
+                R.anim.rotate
+            } else {
+                R.anim.scale
+            }
+
             AnimationUtils.loadAnimation(
                 context,
-                R.anim.rotate
+                this@ComplexListAdapter.animation
             ).also { animation ->
                 startAnimation(
                     animation
                 )
             }
         }
+
+        super.onViewAttachedToWindow(viewHolder)
     }
 
     override fun getItemCount() = dataSet.size
--- MasterAndroid/app/src/main/java/pn/masterandroid/MainActivity.kt
+++ MasterAndroid/app/src/main/java/pn/masterandroid/MainActivity.kt
@@ -2,22 +2,29 @@ package pn.masterandroid
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
-    private val navController
-        get() = findNavController(R.id.nav_host_fragment)
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        setSupportActionBar(findViewById(R.id.toolbar))
-        setupActionBarWithNavController(navController)
-    }
 
-    override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp() || super.onSupportNavigateUp()
+        setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(
+            R.id.nav_host_fragment
+        ) as NavHostFragment
+
+        navController = navHostFragment.navController
+
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        findViewById<BottomNavigationView>(R.id.bottom_navigation_view)
+            .setupWithNavController(navController)
     }
 }
